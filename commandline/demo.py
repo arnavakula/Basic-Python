4bb8ade1ac3cc75ee7f38c90cfacf54279040b8d
Arnav Akula <akula.arnav@gmail.com>
2020-05-28 15:13:29 -0700

    Command line arguments

---
+++ commandline/demo.py
@@ -0,0 +1,9 @@
+import sys
+
+lst = sys.argv
+print(len(lst))
+
+for i in lst:
+    print(i)
+
+#python demo.py 123 456
---
+++ commandline/product.py
@@ -0,0 +1,9 @@
+import sys
+
+lst = sys.argv
+
+prod = int(lst[1]) * int(lst[2])
+print("Product: ", prod)
+
+# lst[0] is the pathname
+# python file_name.py arg1 arg2 ... 
