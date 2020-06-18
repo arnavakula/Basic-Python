816134e0a2ea98e7f08b2112ea9123ac23f5d7c2
arnavakula <akula.arnav@gmail.com>
2020-06-18 12:24:55 -0700

    Inheritance v2

--- oop2/account.py
+++ oop2/account.py
@@ -25,7 +25,21 @@ class Account:
         with open(self.filepath, 'w') as f:
             f.write(str(self.balance))
 
+class CheckingAccount(Account):
+    
+    def __init__(self, filepath):
+        super().__init__(filepath) #no self needed
+        self.user = 'Arnav' #can add additional instance vars
+    
+    def transfer(self, amount, account):
+        self.balance -= amount
+        account += amount
+        self.commit()
 
 acc1 = Account('balance.txt')
 acc1.deposit(100)
 print(acc1.balance)
+
+cacc1 = CheckingAccount('balance.txt')
+cacc1.deposit(100)
+print(cacc1.balance)
